5648f04be78665fec2dca360a296d6f5e6017767
kabindrr <chaina_kabindra@yahoo.com>
2024-09-08 15:47:08 +1000

    react router dom set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Button } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/Login";
+
 const App = () => {
-  return <>hello from react developers point</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
+      <ToastContainer />
+    </>
+  );
 };
 
 export default App;
