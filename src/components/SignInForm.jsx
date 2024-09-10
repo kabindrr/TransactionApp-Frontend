8dbe54115f692f3f3ca093f57c6f66ee25a9cff8
kabindrr <chaina_kabindra@yahoo.com>
2024-09-11 08:26:52 +1000

    signup

---
+++ src/components/SignInForm.jsx
@@ -0,0 +1,55 @@
+import { Button, Form } from "react-bootstrap";
+import { CustomInput } from "./CustomInput";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { postNewUser } from "../helpers/userAxios";
+
+export const SignInForm = () => {
+  const [form, setForm] = useState({});
+
+  const fields = [
+    {
+      label: "Email",
+      placeholder: "johnsmith@gmail.com",
+      required: true,
+      type: "email",
+      name: "email",
+    },
+    {
+      label: "Password",
+      placeholder: "************",
+      required: true,
+      type: "password",
+      name: "password",
+    },
+  ];
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <div className="border rounded p-4">
+      <h3 className="mb-5 text-center ">Keep Track Of Your Finances</h3>{" "}
+      <Form onSubmit={handleOnSubmit}>
+        {fields.map((input) => (
+          <CustomInput key={input.name} {...input} onChange={handleOnChange} />
+        ))}
+
+        <div className="div d-grid">
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { FcMoneyTransfer } from "react-icons/fc";
+import { CustomInput } from "../components/CustomInput";
+import { Form } from "react-router-dom";
+import { BsGraphUpArrow } from "react-icons/bs";
+import { BsGraphDownArrow } from "react-icons/bs";
+import { SignInForm } from "../components/SignInForm";
 
 export const Login = () => {
-  return <div>hey login now with toastify</div>;
+  return (
+    <>
+      <Container className="p-5">
+        <Row className="bg-dark p-5 rounded shadow">
+          <Col md={6}>
+            <SignInForm />
+          </Col>
+          <Col md={6}>
+            <div
+              className="d-flex flex-column justify-content-center "
+              style={{ height: "100%" }}
+            >
+              <div className="text-center p-5 " style={{ fontSize: "1.3rem" }}>
+                <FcMoneyTransfer
+                  className="text-success"
+                  style={{ fontSize: "10rem" }}
+                />
+              </div>
+
+              <div
+                className="text-center text-success p-2"
+                style={{ fontSize: "1.3rem" }}
+              >
+                {" "}
+                <BsGraphUpArrow className="text-success" /> Let your Money Grow
+              </div>
+              <div
+                className="text-center text-decoration-line-through"
+                style={{ fontSize: "1.3rem" }}
+              >
+                {" "}
+                <BsGraphDownArrow className="text-warning " /> Watch Your
+                Spending
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
