046b586cf191333f3d65f861c57d33644ccbdee8
kabindrr <chaina_kabindra@yahoo.com>
2024-10-06 13:06:14 +1100

    transaction form and transaction table

--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -1,11 +1,17 @@
-import { Container, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import { TransactionForm } from "../components/TransactionForm";
+import { TransactionTable } from "../components/TransactionTable";
 
 export const Transaction = () => {
   return (
     <>
       <Container className="p-5">
         <Row className="bg-dark p-5 rounded shadow">
-          Hey i am from transaction
+          <Col>
+            <TransactionForm />
+            <hr />
+            <TransactionTable />
+          </Col>
         </Row>
       </Container>
     </>
