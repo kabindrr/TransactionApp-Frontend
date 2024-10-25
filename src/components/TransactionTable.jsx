9fd03c5e7c222482eeba756ca19b1e7c3f635ad8
kabindrr <chaina_kabindra@yahoo.com>
2024-10-25 14:16:58 +1100

    transaction table

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Table } from "react-bootstrap";
 
 export const TransactionTable = () => {
   return (
-    <div>TransactionTable</div>
-  )
-}
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Date</th>
+          <th>Title</th>
+          <th>Deposit</th>
+          <th>Expenses</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>25/10/2024</td>
+
+          <td>Salary</td>
+          <td>$1000</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>22/10/2024</td>
+          <td>Shopping</td>
+          <td></td>
+          <td>$-630</td>
+        </tr>
+        <tr>
+          <td colSpan={4} className="fw-bold text-center">
+            Total Balance
+          </td>
+          <td className="fw-bold">$470</td>
+        </tr>
+      </tbody>
+    </Table>
+  );
+};
