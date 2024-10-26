26b720df697488acd930fe7f24ee0d881991e8ed
kabindrr <chaina_kabindra@yahoo.com>
2024-10-26 15:18:52 +1100

    transaction with tables from database and reduce for calculations

--- src/components/TransactionTable.jsx
+++ src/components/TransactionTable.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Table } from "react-bootstrap";
+import { useUser } from "../context/UserContext";
 
 export const TransactionTable = () => {
+  const { transactions } = useUser();
+  console.log(transactions);
+  const total = transactions.reduce((acc, item) => {
+    return item.type === "income" ? acc + item.amount : acc - item.amount;
+  }, 0);
   return (
     <Table striped bordered hover>
       <thead>
@@ -14,26 +20,34 @@ export const TransactionTable = () => {
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>25/10/2024</td>
+        {transactions.length > 0 &&
+          transactions.map((t, i) => (
+            <tr key={t._id}>
+              <td>{i + 1}</td>
+              <td>{t.createdAt.slice(0, 10)}</td>
 
-          <td>Salary</td>
-          <td>$1000</td>
-          <td></td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>22/10/2024</td>
-          <td>Shopping</td>
-          <td></td>
-          <td>$-630</td>
-        </tr>
+              <td>{t.title}</td>
+
+              {t.type === "expenses" && (
+                <>
+                  <td></td>
+                  <td>-${t.amount}</td>
+                </>
+              )}
+              {t.type === "income" && (
+                <>
+                  <td>${t.amount}</td>
+                  <td></td>
+                </>
+              )}
+            </tr>
+          ))}
+
         <tr>
           <td colSpan={4} className="fw-bold text-center">
             Total Balance
           </td>
-          <td className="fw-bold">$470</td>
+          <td className="fw-bold">${total}</td>
         </tr>
       </tbody>
     </Table>
