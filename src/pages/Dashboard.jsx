92b6611cdfce4044acdbd82f8a4d49c00f86cc81
kabindrr <chaina_kabindra@yahoo.com>
2024-11-02 16:32:26 +1100

    dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,7 +5,11 @@ export const Dashboard = () => {
   return (
     <>
       <Container className="p-5">
+        
         <Row className="bg-dark p-5 rounded shadow">
+          
+          {/* dashboard update with balance */}
+
           <MyBarChart />
         </Row>
       </Container>
